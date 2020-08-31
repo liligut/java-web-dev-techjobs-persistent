e58e684cd793b189ebdd46b19df2233d91838cc0
Liliana Gutierrez-Hernandez <lgutierrezh79@gmail.com>
2020-08-31 17:31:26 -0400

    queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 ## Part 1: Test it with SQL
-
+SHOW FIELDS FROM job;
 ## Part 2: Test it with SQL
-
+select name from employer where location='St. Louis';
 ## Part 3: Test it with SQL
-
+drop table job;
 ## Part 4: Test it with SQL
+SELECT distinct name, description FROM skill inner join job_skills on skill.id = job_skills.skills_id order by name;
